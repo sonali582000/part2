9eae186936bd301eb7bcf46f1167254decf9b3e0
sonali582000 <rathodsonali393@gmail.com>
2024-03-05 21:47:24 +0100

    part1

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,9 +1,38 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addMember = (event) => {
+    event.preventDefault();
+
+    setNewName(newName);
+    setPersons(persons.concat(newName));
+
+    console.log(persons);
+  };
+
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addMember}>
+        <div>
+          name:{" "}
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {})}
+    </div>
   );
-}
+};
 
 export default App;
