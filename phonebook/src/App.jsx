21671f701c07aaa6feece498e6dd55f5853f7a79
sonali582000 <rathodsonali393@gmail.com>
2024-03-07 19:27:00 +0100

    done

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "010-452365", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Ade Lovelace", number: "39-44-5323523", id: 5 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState();
   const [searchKey, setSearchKey] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      const data = response.data;
+      setPersons(data);
+      console.log(data);
+    });
+  }, []);
 
   const searchPerson = persons.filter((person) =>
     person.name.toLocaleLowerCase().includes(searchKey.toLocaleLowerCase())
